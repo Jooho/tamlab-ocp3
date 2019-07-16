10c497e9bf3dc47487d2aecb9240ae0b5a0be15b
Jooho Lee <ljhiyh@gmail.com>
2019-07-16 12:24:24 -0400

    new-user-process

---
+++ openstack/roles/new-user-process/test/1_test.sh
@@ -0,0 +1,2 @@
+#ansible-playbook --ask-vault-pass -vvvv ./playbooks/test.yaml 
+ansible-playbook --vault-password-file ~/ansible_password -vvvv ./playbooks/test.yaml 
---
+++ openstack/roles/new-user-process/test/2_verify.sh
@@ -0,0 +1,4 @@
+#ansible-playbook --ask-vault-pass -vvvv ./playbooks/verify.yaml
+ansible-playbook --vault-password-file ~/ansible_password -vvvv ./playbooks/verify.yaml
+
+

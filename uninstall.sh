b59744dba8cf613e4b56bb587a640e5a3b555cb1
Jooho Lee <ljhiyh@gmail.com>
2019-08-21 12:06:10 -0400

    update playbook

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,2 @@
-ansible-playbook -i hosts playbooks/uninstall.yaml -vvvv --vault-password-file ~/ansible_password
+ansible-playbook -i hosts playbooks/uninstall.yaml -vvvv --vault-password-file ~/ansible_password -e @vars/all
 

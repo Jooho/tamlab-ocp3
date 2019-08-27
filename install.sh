a28209bf2960f3d1a3aeaa1dc61c6960a24ed184
JooHo Lee <jlee@redhat.com>
2019-08-27 12:20:23 -0400

    Last update

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #ansible-playbook -i hosts --vault-password-file ~/ansible_password playbooks/full-flow.yaml -vvvv -e @./openstack.yaml -e @vars/all -e @openshift.yaml
-ansible-playbook -i hosts --vault-password-file ~/ansible_password playbooks/full-flow.yaml -vvvv -e @./openstack.yaml -e @vars/all -e @openshift.yaml
+ansible-playbook -i hosts --vault-password-file ~/ansible_password playbooks/full-flow.yaml -vvvv -e @./openstack.yaml -e @vars/all -e @openshift.yaml -e ignore_validate=true 
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,3 @@
-ansible-playbook -i hosts playbooks/uninstall.yaml -vvvv --vault-password-file ~/ansible_password -e @vars/all
+#ansible-playbook -i hosts playbooks/uninstall.yaml -vvvv --vault-password-file ~/ansible_password -e @vars/all -e cmd=uninstall 
+ansible-playbook -i hosts playbooks/uninstall.yaml -vvvv --vault-password-file ~/ansible_password -e @./openstack.yaml -e @vars/all -e cmd=uninstall 
 

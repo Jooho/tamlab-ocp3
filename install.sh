f6a53c817a36cf00779eda055a75c1ff0bcee8ad
JooHo Lee <jlee@redhat.com>
2019-09-05 10:28:18 -0400

    Add from-ocp-install param

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #ansible-playbook -i hosts --vault-password-file ~/ansible_password playbooks/full-flow.yaml -vvvv -e @./openstack.yaml -e @vars/all -e @openshift.yaml
-ansible-playbook -i hosts playbooks/full-flow.yaml -vvvv -e @./openstack.yaml -e @vars/all -e @openshift.yaml -e cmd=install
+ansible-playbook -i hosts playbooks/full-flow.yaml -vvvv -e @./openstack.yaml -e @vars/all -e @openshift.yaml -e cmd=install $1
